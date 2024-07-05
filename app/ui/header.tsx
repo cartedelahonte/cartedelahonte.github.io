742027f461ae33b822feaa6ac960f6192dcde814
cartedelahontev2 <cartedelahonte@gmail.com>
2024-07-05 03:43:47 +0200

    Create components

---
+++ app/ui/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      La carte de la honte
+    </div>
+  );
+}
---
+++ app/ui/map.tsx
@@ -0,0 +1,7 @@
+export default function Map() {
+  return (
+    <div>
+      map
+    </div>
+  );
+}
