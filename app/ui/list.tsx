57fdb17c81eb454bb13b15689e0e8c4889a7f0b3
cartedelahontev2 <cartedelahonte@gmail.com>
2024-07-05 16:41:54 +0200

    Scroll to top of container when problem changes

--- app/ui/list.tsx
+++ app/ui/list.tsx
@@ -1,6 +1,7 @@
 import {problems} from "@/app/data/problems";
 import ProblemElement from "@/app/ui/problem-element";
 import ProblemList from "@/app/ui/problem-list";
+import {useEffect, useRef} from "react";
 
 interface ListProps {
   problemId: string|null,
@@ -11,8 +12,16 @@ interface ListProps {
 }
 
 export default function List({problemId, problemType, onClose, setProblemType, setProblemId}: ListProps) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    ref.current!.scroll({
+      top: 0,
+    });
+  }, [problemId, problemType]);
+
   return (
-    <div className="fixed bottom-0 md:bottom-auto rounded-tr-lg rounded-tl-lg md:rounded-bl-lg md:rounded-br-lg left-0 right-0 md:left-[50px] md:top-[50px] md:max-w-[600px] max-h-[45vh] md:max-h-[90vh] z-[1000] bg-white overflow-y-auto">
+    <div ref={ref} className="fixed bottom-0 md:bottom-auto rounded-tr-lg rounded-tl-lg md:rounded-bl-lg md:rounded-br-lg left-0 right-0 md:left-[50px] md:top-[50px] md:max-w-[600px] max-h-[45vh] md:max-h-[90vh] z-[1000] bg-white overflow-y-auto">
       <div className="container mx-auto max-w-[800px] break-words">
         <div className="p-4 md:p-6">
           {null !== problemId ?
